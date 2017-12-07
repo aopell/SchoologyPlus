3a760a7654cbe5ea5da5c7f52db21ab6ccea577c
Glen Husman <wappokickingdeer@gmail.com>
2017-12-07 13:13:01 -0800

    Scripts now inject at earliest possible time

--- js/grades.js
+++ js/grades.js
@@ -1,78 +1,3 @@
-document.body.onload = () => {
-    console.log("Running Schoology Plus grades page improvement script");
-    let inner = document.getElementById("main-inner") || document.getElementById("content-wrapper");
-    let courses = inner.getElementsByClassName("gradebook-course");
-    for (let course of courses) {
-        let title = course.getElementsByClassName("gradebook-course-title")[0];
-        let summary = course.getElementsByClassName("summary-course")[0];
-        let courseGrade = summary.getElementsByClassName("awarded-grade")[0];
-        let grades = course.getElementsByClassName("gradebook-course-grades")[0].firstElementChild.firstElementChild;
-        let categories = grades.getElementsByClassName("category-row");
-        let rows = Array.from(grades.children);
-
-        let classPoints = 0;
-        let classTotal = 0;
-
-        for (let category of categories) {
-            let assignments = rows.filter(x => category.dataset.id == x.dataset.parentId);
-            let sum = 0;
-            let max = 0;
-            for (let assignment of assignments) {
-                let maxGrade = assignment.getElementsByClassName("max-grade")[0];
-                let score = assignment.getElementsByClassName("rounded-grade")[0];
-                if (score) {
-                    let assignmentScore = Number.parseFloat(score.textContent);
-                    let assignmentMax = Number.parseFloat(maxGrade.textContent.substring(3));
-                    sum += assignmentScore;
-                    max += assignmentMax;
-
-                    let newGrade = document.createElement("span");
-                    newGrade.textContent += assignmentMax === 0 ? "EC" : `${Math.round(assignmentScore * 100 / assignmentMax)}%`;
-                    newGrade.classList.add("max-grade");
-                    maxGrade.parentElement.appendChild(document.createElement("br"));
-                    maxGrade.parentElement.appendChild(newGrade);
-                    maxGrade.parentElement.style.padding = "7px 30px 5px";
-                    maxGrade.parentElement.style.textAlign = "center";
-                }
-                else {
-                    let noGrade = assignment.getElementsByClassName("no-grade")[0];
-                    noGrade.parentElement.style.textAlign = "center";
-                    let newGrade = document.createElement("span");
-                    newGrade.textContent += "N/A";
-                    newGrade.classList.add("max-grade");
-                    noGrade.parentElement.appendChild(document.createElement("br"));
-                    noGrade.parentElement.appendChild(newGrade);
-                    noGrade.parentElement.style.padding = "7px 30px 5px";
-                    noGrade.parentElement.style.textAlign = "center";
-                }
-            }
-            let gradeText = category.getElementsByClassName("awarded-grade")[0];
-            setGradeText(gradeText, sum, max, category);
-            let weightText = category.getElementsByClassName("percentage-contrib")[0];
-            if (!weightText) {
-                classPoints += sum;
-                classTotal += max;
-            }
-        }
-
-        let grade = document.createElement("span");
-        grade.classList.add("awarded-grade");
-        grade.style.cssFloat = "right";
-        grade.style.color = courseGrade ? "#3aa406" : "#767676";
-        grade.style.fontWeight = "bold";
-        grade.style.fontSize = "14px";
-        grade.textContent = courseGrade ? courseGrade.textContent : "—";
-        title.appendChild(grade);
-
-        let periods = course.getElementsByClassName("period-row");
-        gradeText = periods[0].getElementsByClassName("awarded-grade")[0];
-        setGradeText(gradeText, classPoints, classTotal, periods[0], classTotal === 0);
-        for (let i = 1; i < periods.length; i++) {
-            periods[i].remove();
-        }
-    }
-};
-
 function setGradeText(gradeElement, sum, max, row, doNotDisplay) {
     if (gradeElement) {
         let text = gradeElement.textContent;
@@ -94,5 +19,78 @@ function setGradeText(gradeElement, sum, max, row, doNotDisplay) {
         span.style.cssFloat = "right"; //maybe remove
         span.style.color = "#3aa406";
         span.style.fontWeight = "bold";
+    }
+}
+
+console.log("Running Schoology Plus grades page improvement script");
+let inner = document.getElementById("main-inner") || document.getElementById("content-wrapper");
+let courses = inner.getElementsByClassName("gradebook-course");
+for (let course of courses) {
+    let title = course.getElementsByClassName("gradebook-course-title")[0];
+    let summary = course.getElementsByClassName("summary-course")[0];
+    let courseGrade = summary.getElementsByClassName("awarded-grade")[0];
+    let grades = course.getElementsByClassName("gradebook-course-grades")[0].firstElementChild.firstElementChild;
+    let categories = grades.getElementsByClassName("category-row");
+    let rows = Array.from(grades.children);
+
+    let classPoints = 0;
+    let classTotal = 0;
+
+    for (let category of categories) {
+        let assignments = rows.filter(x => category.dataset.id == x.dataset.parentId);
+        let sum = 0;
+        let max = 0;
+        for (let assignment of assignments) {
+            let maxGrade = assignment.getElementsByClassName("max-grade")[0];
+            let score = assignment.getElementsByClassName("rounded-grade")[0];
+            if (score) {
+                let assignmentScore = Number.parseFloat(score.textContent);
+                let assignmentMax = Number.parseFloat(maxGrade.textContent.substring(3));
+                sum += assignmentScore;
+                max += assignmentMax;
+
+                let newGrade = document.createElement("span");
+                newGrade.textContent += assignmentMax === 0 ? "EC" : `${Math.round(assignmentScore * 100 / assignmentMax)}%`;
+                newGrade.classList.add("max-grade");
+                maxGrade.parentElement.appendChild(document.createElement("br"));
+                maxGrade.parentElement.appendChild(newGrade);
+                maxGrade.parentElement.style.padding = "7px 30px 5px";
+                maxGrade.parentElement.style.textAlign = "center";
+            }
+            else {
+                let noGrade = assignment.getElementsByClassName("no-grade")[0];
+                noGrade.parentElement.style.textAlign = "center";
+                let newGrade = document.createElement("span");
+                newGrade.textContent += "N/A";
+                newGrade.classList.add("max-grade");
+                noGrade.parentElement.appendChild(document.createElement("br"));
+                noGrade.parentElement.appendChild(newGrade);
+                noGrade.parentElement.style.padding = "7px 30px 5px";
+                noGrade.parentElement.style.textAlign = "center";
+            }
+        }
+        let gradeText = category.getElementsByClassName("awarded-grade")[0];
+        setGradeText(gradeText, sum, max, category);
+        let weightText = category.getElementsByClassName("percentage-contrib")[0];
+        if (!weightText) {
+            classPoints += sum;
+            classTotal += max;
+        }
+    }
+
+    let grade = document.createElement("span");
+    grade.classList.add("awarded-grade");
+    grade.style.cssFloat = "right";
+    grade.style.color = courseGrade ? "#3aa406" : "#767676";
+    grade.style.fontWeight = "bold";
+    grade.style.fontSize = "14px";
+    grade.textContent = courseGrade ? courseGrade.textContent : "—";
+    title.appendChild(grade);
+
+    let periods = course.getElementsByClassName("period-row");
+    gradeText = periods[0].getElementsByClassName("awarded-grade")[0];
+    setGradeText(gradeText, classPoints, classTotal, periods[0], classTotal === 0);
+    for (let i = 1; i < periods.length; i++) {
+        periods[i].remove();
     }
 }
