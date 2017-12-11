65d1ba9c38185a71d4a70eb171dc6f5b0aeee3f2
Aaron Opell <aaron.opell@live.com>
2017-12-10 17:40:26 -0800

    Added notification support Untested Addresses #2

---
+++ js/background.js
@@ -0,0 +1,75 @@
+console.log("Loaded event page");
+console.log("Adding onInstalled listener");
+chrome.runtime.onInstalled.addListener(function () {
+    console.log("Registered alarm");
+    chrome.alarms.create("notification", {
+        periodInMinutes: 5
+    });
+});
+console.log("Adding alarm listener");
+chrome.alarms.onAlarm.addListener(onAlarm);
+
+function onAlarm(alarm) {
+    if (alarm && alarm.name === "notification") {
+        try {
+            console.log(`[${new Date()}] Checking for new notifications`);
+            fetch("https://lms.lausd.net/notifications/ajax", {
+                credentials: "same-origin"
+            }).then(function (response) {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error("Error loading notifications: " + response);
+            }).then(function (response) {
+                chrome.storage.sync.get("lastTime", function (value) {
+                    let time = value.lastTime || Date.now();
+                    let timeModified = false;
+                    let div = document.querySelector("div") || document.body.appendChild(document.createElement("div"));
+                    div.innerHTML = response.output;
+                    let notifications = div.querySelectorAll(".edge-sentence");
+                    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+                    for (let notification of Array.from(notifications).reverse()) {
+                        if (notification.textContent.includes("new grade")) {
+                            let assignments = notification.getElementsByTagName("a");
+                            let extraTextElement = notification.querySelector(".other-items-link");
+                            let timeText = notification.querySelector(".edge-time").textContent;
+                            let monthDay = timeText.split(" at ")[0];
+                            let now = new Date();
+                            let monthDayYear = monthDay + ` ${now.getFullYear()}`;
+                            let today = `${months[now.getMonth()]} ${now.getDate()} ${now.getFullYear()}`;
+                            let notificationDate = Date.parse(monthDayYear);
+                            if (notificationDate > Date.parse(today)) {
+                                notificationDate = Date.parse(monthDay + ` ${now.getFullYear() - 1}`);
+                            }
+
+                            if (notificationDate > time) {
+                                time = notificationDate;
+                                timeModified = true;
+                                let count = 0;
+                                if (extraTextElement) {
+                                    count = +extraTextElement.textContent.match(/\d+/)[0];
+                                }
+                                console.warn("New notification!");
+                                console.warn(notification);
+                                chrome.notifications.create("gradeNotification",{
+                                    type: "basic",
+                                    iconUrl: "imgs/icon@128.png",
+                                    title: "New grade posted",
+                                    message: `${assignments.length + count} new assignment${assignments.length + count === 1 ? " has a grade" : "s have grades"}`,
+                                    eventTime: Date.now()
+                                }, null);
+                            }
+                        }
+                    }
+                    if (timeModified) {
+                        chrome.storage.sync.set({ lastTime: time }, () => { console.log("Set new time " + new Date(time)) });
+                    } else {
+                        console.log("No new notifications");
+                    }
+                });
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
