960db5a0600908ba4e08b16f621ab43562563de5
Glen Husman <wappokickingdeer@gmail.com>
2018-01-28 11:09:21 -0800

    Fix weighted classes with unstated (0%) weights Changes the approach to weighted grade calculation to a single for loop, and Should(TM) handle cases with weird weight totals

--- js/grades.js
+++ js/grades.js
@@ -442,27 +442,39 @@ function createEditListener(gradeColContentWrap, catRow, perRow, finishedCallbac
                 awardedPeriodPercent.textContent = (Math.round(newPerPercent * 100) / 100) + "%";
             } else {
                 let total = 0;
-                let totalPercent = 0;
-                let categories = perRow.parentElement.getElementsByClassName("category-row");
-                for (let category of categories) {
-                    let weightPercent = category.querySelector(".percentage-contrib").textContent;
-                    let roundedGrade = category.querySelector(".rounded-grade");
-                    let maxGrade = category.querySelector(".max-grade");
-                    if (roundedGrade && maxGrade && roundedGrade.textContent != "0" || maxGrade.textContent.slice(3) != "0") {
-                        totalPercent += Number.parseFloat(weightPercent.slice(1, -2));
+                let totalPercentWeight = 0;
+                for (let category of perRow.parentElement.getElementsByClassName("category-row")) {
+                    let weightPercentElement = category.getElementsByClassName("percentage-contrib")[0];
+                    if (!weightPercentElement) {
+                        continue;
                     }
-                }
-                for (let category of categories) {
-                    let weightPercent = category.querySelector(".percentage-contrib").textContent;
-                    let col = category.querySelector(".grade-column-right");
+                    let weightPercent = weightPercentElement.textContent;
+                    let col = category.getElementsByClassName("grade-column-right")[0];
                     let colMatch = col ? col.textContent.match(/(\d+\.?\d*)%/) : null;
                     if (colMatch) {
                         let scorePercent = Number.parseFloat(colMatch[1]);
-                        total += (weightPercent.slice(1, -2) / totalPercent) * scorePercent;
-                        awardedPeriodPercent.title = total + "%";
-                        awardedPeriodPercent.textContent = (Math.round(total * 100) / 100) + "%";
+                        if (scorePercent && !Number.isNaN(scorePercent)) {
+                            total += (weightPercent.slice(1, -2) / 100) * scorePercent;
+                            totalPercentWeight += Number.parseFloat(weightPercent.slice(1, -2));
+                        }
                     }
                 }
+
+                totalPercentWeight /= 100;
+
+                // if only some categories have assignments, adjust the total accordingly 
+                // if weights are more than 100, this assumes that it's correct as intended (e.c.), I won't mess with it
+                if (totalPercentWeight > 0 && totalPercentWeight < 1) {
+                    // some categories are specified, but weights don't quite add to 100
+                    // scale up known grades
+                    total /= totalPercentWeight;
+                // epsilon because floating point
+                } else if (totalPercentWeight < 0.00001) {
+                    total = 100;
+                }
+
+                awardedPeriodPercent.title = total + "%";
+awardedPeriodPercent.textContent = (Math.round(total * 100) / 100) + "%";
             }
 
             if (!awardedPeriodPercentContainer.querySelector(".modified-score-percent-warning")) {
