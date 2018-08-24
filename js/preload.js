e1ed5137d6407b75046b3eb1de3cd11b0eed92de
Glen Husman <wappokickingdeer@gmail.com>
2018-08-24 07:28:42 -0700

    Replace tempDiv with DOMParser for API key generation Should hopefully reduce bugs: now if something goes wrong instead of a null we'll get an infinite loop untested, as I can't clear my account of an API key

--- js/preload.js
+++ js/preload.js
@@ -464,22 +464,24 @@ async function getApiKeys() {
     let userId = document.querySelector("#profile > a").href.match(/\d+/)[0];
     var apiKeys = null;
     console.log(`Fetching API key for user ${userId}`);
-    let tempDiv = document.createElement("div");
     let html = await (await fetch("https://lms.lausd.net/api", { credentials: "same-origin" })).text();
-    tempDiv.innerHTML = html;
+    let docParser = new DOMParser();
+    let doc = docParser.parseFromString(html, "text/html");
+
     let key;
     let secret;
-    if ((key = tempDiv.querySelector("#edit-current-key")) && (secret = tempDiv.querySelector("#edit-current-secret"))) {
+    if ((key = doc.getElementById("edit-current-key")) && (secret = doc.getElementById("edit-current-secret"))) {
         console.log("API key already generated - storing");
         apiKeys = [key.value, secret.value, userId];
-        localStorage.setObject("attemptedGetKey", null);
-    } else if (localStorage.getObject("attemptedGetKey") != userId) {
-        console.log("API key not found - generating and reloading page");
-        localStorage.setObject("attemptedGetKey", userId);
-        document.body.appendChild(tempDiv);
-        tempDiv.querySelector("input[type=submit]").click();
-        location.reload();
-        return null;
+    } else {
+        console.log("API key not found - generating and trying again");
+        let submitData = new FormData(document.getElementById("s-api-register-form"));
+        let generateFetch = await fetch("https://lms.lausd.net/api", { 
+            credentials: "same-origin",
+            body: submitData,
+            method: "post" });
+        console.log(`Generatekey response: ${generateFetch.status}`);
+        return getApiKeys();
     }
 
     return apiKeys;
