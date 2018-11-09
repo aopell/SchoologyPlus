c2ca16f40a0a44ad1fa282ab9ed0813dd2557a62
Glen Husman <wappokickingdeer@gmail.com>
2018-11-08 17:37:26 -0800

    Document tooltips now appear on name, not filesize, hover More intuitive and discoverable this way

--- js/materials.js
+++ js/materials.js
@@ -112,7 +112,7 @@
         loadedTextHolder.dataset.tooltipHtml = "Loading...";
         // title already has a tooltip (full filename), so we'll use the filesize instead
         // if there's no filesize (e.g. because this document isn't a file), we won't create the tooltip, so no problems
-        $(value).find(".attachments-file-name .attachments-file-size").tipsy({ gravity: "w", html: true, title: () => loadedTextHolder.dataset.tooltipHtml });
+        $(value).find(".attachments-file-name>a").tipsy({ gravity: "w", html: true, title: () => loadedTextHolder.dataset.tooltipHtml });
 
         value.dataset.schoologyPlusProcessedTooltip = true;
     }
