bbc6e601fbe5e4ee4fd85df20666b77e209a3643
Glen Husman <wappokickingdeer@gmail.com>
2018-12-03 21:42:11 -0800

    PoC advanced settings page that works(TM) Needs to be tested in more realistic environments

--- js/advanced-settings.js
+++ js/advanced-settings.js
@@ -1,12 +1,34 @@
-console.log("okthen");
+console.log("Advanced settings init");
 
-window.onload = function() {
-    console.log("init called");
-    document.getElementById("request-cookies").addEventListener("click", event => {
-        chrome.permissions.request({ permissions: ["cookies"] }, function (granted) {
-            if (granted) {
-                chrome.storage.sync.set({ sessionCookiePersist: "enabled" }, () => console.log("DONE ENABLING SESSIONS"));
-            }
-        });
+// FIXME change this signature to take the container element or something
+// for toggles we want to disable the current-setting button
+function setSetting(realName, friendlyName, newState, reasonString = undefined) {
+    chrome.storage.sync.set({ [realName]: newState }, () => {
+        let alertString = friendlyName + " is now " + newState + ".";
+        if (reasonString) {
+            alertString = reasonString + " " + alertString;
+        }
+        alert(alertString);
     });
 }
+
+document.getElementById("stayloggedin-enable").addEventListener("click", event => {
+    chrome.permissions.request({ permissions: ["cookies"] }, function (granted) {
+        if (granted) {
+            chrome.runtime.sendMessage({ type: "install_handler", handlerType: "cookies" }, function (response) {
+                if (!response.isRegistered) {
+                    console.error("Error registering cookie handler: " + response.error);
+                }
+                setSetting("sessionCookiePersist", "Stay Logged In", "enabled");
+            });
+        } else {
+            setSetting("sessionCookiePersist", "Stay Logged In", "disabled", "Required permissions have been denied.");
+        }
+    });
+});
+
+document.getElementById("stayloggedin-disable").addEventListener("click", event => {
+    chrome.runtime.sendMessage({ type: "revoke_permission", permissionSpecification: { permissions: ["cookies"] } }, function (response) {
+        setSetting("sessionCookiePersist", "Stay Logged In", "disabled");
+    });
+});
