c1efaf1c84feeca92ec86d000be4a1d4089129f1
Aaron Opell <aaron.opell@live.com>
2019-03-28 18:39:57 -0700

    Added course icons and context menu to courses page Closes #151

---
+++ js/courses.js
@@ -0,0 +1,43 @@
+for (let course of document.querySelectorAll("li.course-item.list-item")) {
+    let parent = course.parentNode;
+    let wrapper = document.createElement("div");
+    parent.replaceChild(wrapper, course);
+    wrapper.appendChild(course);
+    course.prepend(createElement("img", ["course-list-icon"], { src: Theme.getIcon(course.querySelector(".course-title").textContent) }));
+}
+
+$.contextMenu({
+    selector: "li.course-item.list-item",
+    items: {
+        options: {
+            name: "Course Options",
+            callback: function (key, opt) {
+                openModal("course-settings-modal", {
+                    courseId: this[0].querySelector(".section-item.student-section").id.match(/\d+/)[0],
+                    courseName: `${this[0].querySelector(".course-title").textContent}: ${this[0].querySelector(".section-item.student-section").textContent}`
+                });
+            }
+        },
+        separator: "-----",
+        materials: {
+            name: "Materials",
+            callback: function (key, opt) { window.open(`https://lms.lausd.net/course/${this[0].querySelector(".section-item.student-section").id.match(/\d+/)[0]}/materials`, "_blank") }
+        },
+        updates: {
+            name: "Updates",
+            callback: function (key, opt) { window.open(`https://lms.lausd.net/course/${this[0].querySelector(".section-item.student-section").id.match(/\d+/)[0]}/updates`, "_blank") }
+        },
+        student_grades: {
+            name: "Grades",
+            callback: function (key, opt) { window.open(`https://lms.lausd.net/course/${this[0].querySelector(".section-item.student-section").id.match(/\d+/)[0]}/student_grades`, "_blank") }
+        },
+        mastery: {
+            name: "Mastery",
+            callback: function (key, opt) { window.open(`https://lms.lausd.net/course/${this[0].querySelector(".section-item.student-section").id.match(/\d+/)[0]}/mastery`, "_blank") }
+        },
+        members: {
+            name: "Members",
+            callback: function (key, opt) { window.open(`https://lms.lausd.net/course/${this[0].querySelector(".section-item.student-section").id.match(/\d+/)[0]}/members`, "_blank") }
+        }
+    }
+});
