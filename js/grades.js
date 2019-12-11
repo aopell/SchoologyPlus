e6918a2300a0865178cec056ed6660f691a9dcef
Aaron Opell <aaron.opell@live.com>
2019-12-11 14:50:30 -0800

    Added new setting to hide help center; other settings improved

--- js/grades.js
+++ js/grades.js
@@ -298,7 +298,7 @@ var fetchQueue = [];
             setGradeText(gradeText, classPoints, classTotal, periods[0], classTotal === 0);
 
             // add weighted indicator to the course section row
-            if (Setting.getValue("weightedGradebookIndicator") == "enabled" && classTotal === 0 && !addMoreClassTotal) {
+            if (classTotal === 0 && !addMoreClassTotal) {
                 let newElem = createElement("span", ["splus-weighted-gradebook-indicator"], {
                     textContent: "[Weighted]"
                 });
