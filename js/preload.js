89156401bdf576e7f0e4dfa9077c58fd10fe3969
Aaron Opell <aaron.opell@live.com>
2020-06-09 17:06:49 -0700

    Add page identifier; Improved global dark theme CSS

--- js/preload.js
+++ js/preload.js
@@ -37,6 +37,7 @@ var Logger = {
 // Process options
 Logger.log(`Loaded Schoology Plus version ${chrome.runtime.getManifest().version}${getBrowser() != "Chrome" || chrome.runtime.getManifest().update_url ? '' : ' (development version)'}`);
 var firstLoad = true;
+document.documentElement.setAttribute("page", location.pathname);
 updateSettings();
 
 var beta_tests = {
